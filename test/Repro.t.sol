c4b53470afe77bd28769c40bb31a0168b1500345
zerosnacks <zerosnacks@protonmail.com>
2025-12-18 15:36:24 +0100

    bump test, flaky panic

--- test/Repro.t.sol
+++ test/Repro.t.sol
@@ -10,22 +10,29 @@ contract ReproTest is Test {
     Vault vault;
     TTT ttt;
 
-    function setUp() public {
-        vault = new Vault(address(this));
+    address constant SENDER = address(0x2f05B1546B2E5170DB125708b228E03573ecc5E0);
 
-        ttt = new TTT(address(this));
-        ttt.mint(address(this), 1_000_000e18);
+    function setUp() public {
+        vm.startPrank(SENDER);
+        vault = new Vault(SENDER);
+        ttt = new TTT(SENDER);
+        ttt.mint(SENDER, 1_000_000e18);
+        vm.stopPrank();
     }
 
     function testDepositWithdrawTTT() public {
+        vm.startPrank(SENDER);
         ttt.approve(address(vault), type(uint256).max);
         vault.depositTokens(address(ttt), 500_000e18);
         vault.withdrawTokens(address(ttt), 500_000e18);
+        vm.stopPrank();
     }
 
     function testDepositWithdrawAlphaUSD() public {
+        vm.startPrank(SENDER);
         StdTokens.ALPHA_USD.approve(address(vault), type(uint256).max);
         vault.depositTokens(address(StdTokens.ALPHA_USD), 1);
         vault.withdrawTokens(address(StdTokens.ALPHA_USD), 1);
+        vm.stopPrank();
     }
 }
